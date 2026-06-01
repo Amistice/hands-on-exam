914da1e9a38d1337bb3cdd9141ac96948c7cd58f
Tayrine <tayrinehz@gmail.com>
2026-06-01 16:23:53 -0300

    feat: remove duplicates and sort array (Q7)

--- section3_practical/question7.py
+++ section3_practical/question7.py
@@ -5,4 +5,4 @@ def get_distinct_sorted(numbers: list) -> list:
 if __name__ == "__main__":
     numbers = [2, 20, 3, 7, 10, 4, 4, 7, 9, 12, 15, 14, 11, 10, 13, 10, 20, 16, 12, 16]
     result = get_distinct_sorted(numbers)
-    print(result)
+    print(result)# Q7: Remove duplicates and sort
